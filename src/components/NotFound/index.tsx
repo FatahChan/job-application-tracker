5939d2f5f6004763a291c9abc3763e38a35593fc
FatahChan <ahmadfathallah89@gmail.com>
2024-07-03 00:08:04 +0300

    chore: change window.location to routerState.location

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,13 +1,15 @@
-import { useNavigate } from "@tanstack/react-router";
+import { useNavigate, useRouterState } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 function NotFound() {
   const navigate = useNavigate();
+  const routerState = useRouterState();
   useEffect(() => {
     // Redirect to the root if the user is on the index page
-    if (window.location.pathname === "/index.html") navigate({ to: "/" });
-  }, [navigate]);
-  if (window.location.pathname === "/index.html") return null;
+    if (routerState.location.pathname === "/index.html") navigate({ to: "/" });
+  }, [navigate, routerState.location.pathname]);
+
+  if (routerState.location.pathname === "/index.html") return null;
 
   return <>Not Found</>;
 }
